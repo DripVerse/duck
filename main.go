991311fc0aa86553aabac450bc7b975c04575690
Kumar Anirudha <mail@anirudha.dev>
2023-11-17 23:42:03 +0530

    username as address

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 	"time"
 
@@ -33,67 +35,69 @@ func init() {
 }
 
 func main() {
-	logrus.Infoln("🚀")
-	// // Define input flags
+	// Define input flags
+	// Fetch EVM Address from Key
+	username := src.GetAddress(viper.GetString("ACCOUNT_KEY_EVM"))
+	// logrus.Infoln("👋", username)
 	// username := flag.String("user", "", "username to use in the chatroom.")
-	// chatroom := flag.String("room", "", "chatroom to join.")
-	// loglevel := flag.String("log", "", "level of logs to print.")
-	// discovery := flag.String("discover", "", "method to use for discovery.")
-	// // Parse input flags
-	// flag.Parse()
+	chatroom := flag.String("room", "", "chatroom to join.")
+	loglevel := flag.String("log", "", "level of logs to print.")
+	discovery := flag.String("discover", "", "method to use for discovery.")
+	// Parse input flags
+	flag.Parse()
 
-	// // Set the log level
-	// switch *loglevel {
-	// case "panic", "PANIC":
-	// 	logrus.SetLevel(logrus.PanicLevel)
-	// case "fatal", "FATAL":
-	// 	logrus.SetLevel(logrus.FatalLevel)
-	// case "error", "ERROR":
-	// 	logrus.SetLevel(logrus.ErrorLevel)
-	// case "warn", "WARN":
-	// 	logrus.SetLevel(logrus.WarnLevel)
-	// case "info", "INFO":
-	// 	logrus.SetLevel(logrus.InfoLevel)
-	// case "debug", "DEBUG":
-	// 	logrus.SetLevel(logrus.DebugLevel)
-	// case "trace", "TRACE":
-	// 	logrus.SetLevel(logrus.TraceLevel)
-	// default:
-	// 	logrus.SetLevel(logrus.InfoLevel)
-	// }
+	// Set the log level
+	switch *loglevel {
+	case "panic", "PANIC":
+		logrus.SetLevel(logrus.PanicLevel)
+	case "fatal", "FATAL":
+		logrus.SetLevel(logrus.FatalLevel)
+	case "error", "ERROR":
+		logrus.SetLevel(logrus.ErrorLevel)
+	case "warn", "WARN":
+		logrus.SetLevel(logrus.WarnLevel)
+	case "info", "INFO":
+		logrus.SetLevel(logrus.InfoLevel)
+	case "debug", "DEBUG":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "trace", "TRACE":
+		logrus.SetLevel(logrus.TraceLevel)
+	default:
+		logrus.SetLevel(logrus.InfoLevel)
+	}
 
-	// // Display the welcome figlet
-	// fmt.Printf(figlet)
-	// fmt.Println("Initiating Duck Chat...")
-	// fmt.Println("(This may take upto 30 seconds.)")
-	// fmt.Println()
+	// Display the welcome figlet
+	fmt.Printf(figlet)
+	fmt.Println("Initiating Duck Chat...")
+	fmt.Println("(This may take upto 30 seconds.)")
+	fmt.Println()
 
-	// // Create a new P2PHost
-	// p2phost := src.NewP2P()
-	// logrus.Infoln("Preparing system...")
+	// Create a new P2PHost
+	p2phost := src.NewP2P()
+	logrus.Infoln("Preparing system...")
 
-	// // Connect to peers with the chosen discovery method
-	// switch *discovery {
-	// case "announce":
-	// 	p2phost.AnnounceConnect()
-	// case "advertise":
-	// 	p2phost.AdvertiseConnect()
-	// default:
-	// 	p2phost.AdvertiseConnect()
-	// }
-	// logrus.Infoln("Finding Anons...")
+	// Connect to peers with the chosen discovery method
+	switch *discovery {
+	case "announce":
+		p2phost.AnnounceConnect()
+	case "advertise":
+		p2phost.AdvertiseConnect()
+	default:
+		p2phost.AdvertiseConnect()
+	}
+	logrus.Infoln("Finding Anons...")
 
-	// // Join the chat room
-	// chatapp, _ := src.JoinChatRoom(p2phost, *username, *chatroom)
-	// logrus.Infof("Joining '%s' as '%s'", chatapp.RoomName, chatapp.UserName)
+	// Join the chat room
+	chatapp, _ := src.JoinChatRoom(p2phost, username, *chatroom)
+	logrus.Infof("Joining '%s' as '%s'", chatapp.RoomName, chatapp.UserName)
 
-	// // Wait for network setup to complete
-	// time.Sleep(time.Second * 5)
+	// Wait for network setup to complete
+	time.Sleep(time.Second * 5)
 
-	// // Create the Chat UI
-	// ui := src.NewUI(chatapp)
-	// // Start the UI system
-	// ui.Run()
+	// Create the Chat UI
+	ui := src.NewUI(chatapp)
+	// Start the UI system
+	ui.Run()
 }
 
 func VerifyConfig() {
@@ -119,7 +123,6 @@ func VerifyConfig() {
 	if len(viper.GetString("ACCOUNT_KEY_EVM")) == 0 {
 		panic("No Wallet Key Found! Kindly update your key before continuing...")
 	}
-	// Fetch EVM Address from Key
-	src.GetAddress(viper.GetString("ACCOUNT_KEY_EVM"))
-	logrus.Infoln("✅ Config OK")
+
+	logrus.Infoln("⚙️", "Config OK")
 }
--- src/web3.go
+++ src/web3.go
@@ -3,47 +3,21 @@ package src
 import (
 	"crypto/ecdsa"
 	"log"
-	"reflect"
 
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/mr-tron/base58"
 )
 
-func wifToPrivateKey(wif string) (*ecdsa.PrivateKey, error) {
-	decoded, err := base58.Decode(wif)
-	if err != nil {
-		return nil, err
-	}
-
-	// The first byte is the version byte, and the last 4 bytes are the checksum.
-	// We skip the version byte and take all but the last 4 bytes.
-	privateKeyBytes := decoded[1 : len(decoded)-4]
-
-	privateKey, err := crypto.ToECDSA(privateKeyBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	return privateKey, nil
-}
-
-func GetAddress(key string) {
-	log.Println("key:", key)
-
-	privateKey, err := wifToPrivateKey(key)
-
-	// privateKey, err := crypto.HexToECDSA(privateKeyHex)
+func GetAddress(key string) string {
+	// log.Println("key:", key, reflect.TypeOf(key))
+	privateKey, err := crypto.HexToECDSA(key)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
 		log.Fatal("error casting public key to ECDSA")
 	}
-
-	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
-
-	log.Println("Address: %s\n", address, reflect.TypeOf(address))
+	address := crypto.PubkeyToAddress(*publicKeyECDSA)
+	return address.Hex()
 }
